be0fe1133d0bbbbfbe5c8480cb131e51abac3d9b
KemalBekir <95451696+KemalBekir@users.noreply.github.com>
2022-08-18 16:09:44 +0100

    Programming Fundamentals - Exercise

---
+++ smallestOfThreeNumber.js
@@ -0,0 +1,8 @@
+function smallestOfThreeNumbers(first,second,third){
+    let minNum = Math.min(first,second,third);
+    console.log(minNum);    
+}
+
+smallestOfThreeNumbers(2,5,3)
+smallestOfThreeNumbers(600,342,123)
+smallestOfThreeNumbers(25,21,4)
