31b5579ab26d57ccc7005d439b26e001b14300e7
KemalBekir <95451696+KemalBekir@users.noreply.github.com>
2022-08-27 16:27:44 +0100

    Programming Fundamentals - Exercise

---
+++ charsToString.js
@@ -0,0 +1,8 @@
+function charsToString(firstChar, secondChar, thirdChar){
+    let result = firstChar + secondChar + thirdChar;
+    console.log(result);
+}
+
+charsToString('a','b','c')
+charsToString('%','2','o')
+charsToString('1','5','p')
