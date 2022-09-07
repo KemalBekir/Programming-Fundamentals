5ba68fcecf601399bbde4cbd742d07020fc2e1ed
KemalBekir <95451696+KemalBekir@users.noreply.github.com>
2022-09-07 08:34:25 +0100

    Programming Fundamentals - Exercise

---
+++ blackFlag.js
@@ -0,0 +1,34 @@
+function flag(input) {
+    let days = Number(input.shift());
+    //console.log(days);
+    let plunderForDay = Number(input.shift());
+    //console.log(plunderForDay);
+    let expectedPlunder = Number(input.shift());
+    //console.log(expectedPlunder);
+    let total= 0;
+    for (let i = 1; i <= days; i++) {
+        total += plunderForDay;
+        if (i % 3 === 0) {
+            total += plunderForDay * 0.5;
+        }
+        if (i % 5 === 0) {
+            total *= 0.7;
+        }
+    }
+    if (total >= expectedPlunder) {
+        console.log(`Ahoy! ${total.toFixed(2)} plunder gained.`);
+    } else {
+        let percentage = total / expectedPlunder * 100;
+        console.log(`Collected only ${percentage.toFixed(2)}% of the plunder.`);
+    }
+}
+
+blackFlag((["5",
+    "40",
+    "100"]))
+blackFlag((["0",
+    "0",
+    "68.94"]))
+blackFlag((["10",
+    "20",
+    "380"]))
