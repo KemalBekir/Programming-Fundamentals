0d4627de24671b6b0a3ec47b6ae1924f28e6479f
KemalBekir <95451696+KemalBekir@users.noreply.github.com>
2022-09-16 17:18:11 +0100

    Programming Fundamentals - Exercise

---
+++ shoppingList.js
@@ -0,0 +1,74 @@
+function shoppingList(input) {
+    let groceries = input.shift().split('!');
+    let line = input.shift();
+
+    while (line != 'Go Shopping!') {
+        let [command, item, newItem] = line.split(' ');
+
+        switch (command) {
+            case 'Urgent':
+                urgent(groceries, item);
+                break;
+            case 'Unnecessary':
+                unnecessary(groceries, item);
+                break;
+            case 'Correct':
+                correct(groceries, item, newItem);
+                break;
+            case 'Rearrange':
+                rearrange(groceries, item);
+                break;
+        }
+
+        line = input.shift();
+    }
+
+    console.log(groceries.join(', '));
+
+    function urgent(list, item) {
+        if (list.includes(item) == false) {
+            list.unshift(item);
+        }
+    }
+
+    function unnecessary(list, item) {
+        if (list.includes(item) == true) {
+            let index = list.indexOf(item);
+            list.splice(index, 1);
+        }
+    }
+ 
+    function correct(list, item, newItem) {
+        if (list.includes(item) == true) {
+            let index = list.indexOf(item);
+            list[index] = newItem;
+        }
+    }
+  
+    function rearrange(list, item) {
+        if (list.includes(item) == true) {
+            let index = list.indexOf(item);
+            list.splice(index, 1);
+            list.push(item);
+        }
+    }
+}
+
+// shoppingList((["Tomatoes!Potatoes!Bread",
+//     "Unnecessary Milk",
+//     "Urgent Tomatoes",
+//     "Go Shopping!"]))
+// shoppingList((["Milk!Pepper!Salt!Water!Banana",
+//     "Urgent Salt",
+//     "Unnecessary Grapes",
+//     "Correct Pepper Onion",
+//     "Rearrange Grapes",
+//     "Correct Tomatoes Potatoes",
+//     "Go Shopping!"]))
+shoppingList((["Milk!Pepper!Salt!Water!Banana",
+    "Urgent Salt",
+    "Unnecessary Water",
+    "Correct Pepper Onion",
+    "Rearrange Milk",
+    "Correct Tomatoes Potatoes",
+    "Go Shopping!"]))
